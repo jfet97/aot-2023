de1142b290d11243e516c0ffaf141ad6cc703516
Andrea Simone Costa <andrysimo1997@gmail.com>
2023-12-19 09:19:29 +0100

    Refactor type definitions in day18.ts

--- day18.ts
+++ day18.ts
@@ -1,17 +1,17 @@
 // Perdoname Madre por Mi Vida Loka
 
 type UnionToIntersection<U> =
-  (U extends any ? (k: U) => void : never) extends ((k: infer I) => void) ? I : never
+  (U extends any ? (k: U) => void : never) extends ((k: infer I) => void) ? I : never;
 
 type LastOf<T> =
-  UnionToIntersection<T extends any ? () => T : never> extends () => (infer R) ? R : never
+  UnionToIntersection<T extends any ? () => T : never> extends () => (infer R) ? R : never;
 
 type Push<T extends any[], V> = [...T, V];
 
 type TuplifyUnion<T, L = LastOf<T>, N = [T] extends [never] ? true : false> =
-  true extends N ? [] : Push<TuplifyUnion<Exclude<T, L>>, L>
+  true extends N ? [] : Push<TuplifyUnion<Exclude<T, L>>, L>;
 
 
 type Count<List, Item> = TuplifyUnion<keyof {
 	[El in keyof List as List[El] extends Item ? El : never]: any
-}>["length"]
+}>["length"];
