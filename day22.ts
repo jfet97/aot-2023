ffb45b0c257e5ef108da7bfb75989966f54bd83c
Andrea Simone Costa <andrysimo1997@gmail.com>
2023-12-22 11:05:03 +0100

    22

---
+++ day22.ts
@@ -0,0 +1,174 @@
+/** because "dashing" implies speed */
+type Dasher = '💨';
+
+/** representing dancing or grace */
+type Dancer = '💃';
+
+/** a deer, prancing */
+type Prancer = '🦌';
+
+/** a star for the dazzling, slightly mischievous Vixen */
+type Vixen = '🌟';
+
+/** for the celestial body that shares its name */
+type Comet = '☄️';
+
+/** symbolizing love, as Cupid is the god of love */
+type Cupid = '❤️';
+
+/** representing thunder, as "Donner" means thunder in German */
+type Donner = '🌩️';
+
+/** meaning lightning in German, hence the lightning bolt */
+type Blitzen = '⚡';
+
+/** for his famous red nose */
+type Rudolph = '🔴';
+
+type Reindeer = Dasher | Dancer | Prancer | Vixen | Comet | Cupid | Donner | Blitzen | Rudolph;
+
+type Tuple3Items = [Reindeer, Reindeer, Reindeer];
+type Tuple9Items = [Tuple3Items, Tuple3Items, Tuple3Items];
+type Tuple9ItemsFlattened = [...Tuple3Items, ...Tuple3Items, ...Tuple3Items];
+type SudokuGrid = [
+	Tuple9Items, 
+	Tuple9Items, 
+	Tuple9Items, 
+	Tuple9Items, 
+	Tuple9Items, 
+	Tuple9Items, 
+	Tuple9Items, 
+	Tuple9Items, 
+	Tuple9Items
+];
+type SudokuGridFlattened = [
+	Tuple9ItemsFlattened,
+	Tuple9ItemsFlattened,
+	Tuple9ItemsFlattened,
+	Tuple9ItemsFlattened,
+	Tuple9ItemsFlattened,
+	Tuple9ItemsFlattened,
+	Tuple9ItemsFlattened,
+	Tuple9ItemsFlattened,
+	Tuple9ItemsFlattened
+];
+
+// utils
+
+type UnionToIntersection<U> = 
+  (U extends any ? (x: U)=>void : never) extends ((x: infer I)=>void) ? I : never;
+
+type Resolve<T> = {[ K in keyof T]: Resolve<T[K]>} & unknown;
+
+//
+
+type ValidTuple<R extends Reindeer, $U extends Reindeer = R> =
+	[R] extends [never]
+		? []
+		: R extends any 
+			? [R, ...ValidTuple<Exclude<$U, R>>]
+			: never;
+
+type _Valid9Items<
+	R extends Reindeer = Reindeer,
+	$CL extends Reindeer[] = [],
+	$CG extends Reindeer[] = [],
+	$U extends Reindeer = R,
+> =
+	[R, $CL] extends [never, []]
+		? true
+		: R extends any 
+			? $CG["length"] extends 6
+				? { 
+						[K in `${$CL[0]}${$CL[1]}${$CL[2]}`]: ValidTuple<$U>
+					}
+				: $CL["length"] extends 3
+						? { 
+								[K in `${$CL[0]}${$CL[1]}${$CL[2]}`]: UnionToIntersection<_Valid9Items<$U, [], $CG>>
+							}
+						: _Valid9Items<Exclude<$U, R>, [...$CL, R], [...$CG, R]>
+			: never;
+
+type Valid9Items = Resolve<UnionToIntersection<_Valid9Items>>;
+
+type Is9TupleValid<
+	T extends Tuple9ItemsFlattened
+> = Valid9Items[`${T[0]}${T[1]}${T[2]}` & keyof Valid9Items] extends infer FirstAccess
+	? [FirstAccess] extends [never]
+		? false
+		: FirstAccess[`${T[3]}${T[4]}${T[5]}` & keyof FirstAccess] extends infer SecondAccess
+			? [SecondAccess] extends [never]
+				? false
+				: [T[6], T[7], T[8]] extends SecondAccess
+					? true
+					: false
+			: false
+	: false;
+
+type Flatten3x3Slice<
+	S extends [Tuple3Items, Tuple3Items, Tuple3Items]
+> = [...S[0], ...S[1], ...S[2]];
+
+type FlattenGrid<Grid extends SudokuGrid> = {
+	[I in keyof Grid]: Flatten3x3Slice<Grid[I]>
+};
+
+type ValidateRows<Grid extends SudokuGrid> = 
+	FlattenGrid<Grid> extends infer $Grid extends SudokuGridFlattened
+		? {
+				[I in keyof $Grid]: Is9TupleValid<$Grid[I]>
+			}[number] extends true ? true : false
+		: false;
+
+type ValidateColumns<Grid extends SudokuGrid> = 
+	FlattenGrid<Grid> extends infer $Grid extends SudokuGridFlattened
+		? {
+				[I in keyof $Grid]: I extends keyof $Grid[number]
+					? Is9TupleValid<
+							[
+								$Grid[0][I], $Grid[1][I], $Grid[2][I],
+								$Grid[3][I], $Grid[4][I], $Grid[5][I],
+								$Grid[6][I], $Grid[7][I], $Grid[8][I]
+							]
+						>
+					: false
+			}[number] extends true ? true : false
+		: false;
+
+type SubgridsCoordinates = [
+	[[0, 0], [1, 0], [2, 0]],
+	[[0, 1], [1, 1], [2, 1]],
+	[[0, 2], [1, 2], [2, 2]],
+	[[3, 0], [4, 0], [5, 0]],
+	[[3, 1], [4, 1], [5, 1]],
+	[[3, 2], [4, 2], [5, 2]],
+	[[6, 0], [7, 0], [8, 0]],
+	[[6, 1], [7, 1], [8, 1]],
+	[[6, 2], [7, 2], [8, 2]],
+];
+
+type ValidateSubgrids<Grid extends SudokuGrid> = 
+	{
+		[I in keyof Grid]: I extends keyof SubgridsCoordinates
+			? SubgridsCoordinates[I] extends infer $Subgrid extends SubgridsCoordinates[number]
+				? Flatten3x3Slice<[
+					Grid[$Subgrid[0][0]][$Subgrid[0][1]],
+					Grid[$Subgrid[1][0]][$Subgrid[1][1]],
+					Grid[$Subgrid[2][0]][$Subgrid[2][1]],
+				]>
+				: false
+			: false
+	} extends infer $Subgrids extends SudokuGridFlattened
+		? {
+				[I in keyof $Subgrids]: Is9TupleValid<$Subgrids[I]>
+			}[number] extends true ? true : false
+		: false;
+
+type Validate<Grid extends SudokuGrid> =
+	ValidateRows<Grid> extends infer $AreRowsValid extends true
+		? ValidateColumns<Grid> extends infer $AreColumnsValid extends $AreRowsValid
+			? ValidateSubgrids<Grid> extends infer $AreSubgridsValid extends $AreColumnsValid
+				? $AreSubgridsValid
+				: false
+			: false
+		: false;
