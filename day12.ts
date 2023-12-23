3b19bc16e1cb45b5c092e5e88bc7da309e37d7be
Andrea Simone Costa <andrysimo1997@gmail.com>
2023-12-23 22:32:22 +0100

    12

--- day12.ts
+++ day12.ts
@@ -3,3 +3,9 @@ type FindSanta<A, R extends readonly any[] = []> = A extends readonly [infer F, 
 		? R["length"]
 		: FindSanta<Rest, [...R, any]>
 	: never;
+
+type ParseNumber<S> = S extends `${infer N extends number}` ? N : never
+
+type FindSantaIt<A extends readonly any[]> = {
+	[I in keyof A]: A[I] extends '🎅🏼' ? ParseNumber<I> : never
+}[number];
