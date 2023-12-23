76d30d4e730b0a0c98aa9fcf572798c04410eacd
Andrea Simone Costa <andrysimo1997@gmail.com>
2023-12-23 15:59:16 +0100

    10

--- day10.ts
+++ day10.ts
@@ -1,7 +1,7 @@
 type StreetSuffixTester<S, SF extends string> =
 	S extends ''
 		? false
-		: S extends SF 
+		: S extends SF
 			? true
 			: S extends `${infer _}${infer Rest}`
 				? StreetSuffixTester<Rest, SF>
