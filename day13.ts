355ffb5a5255f0d9ad42c232d0d543a3e9914555
Andrea Simone Costa <andrysimo1997@gmail.com>
2023-12-23 23:03:26 +0100

    13

--- day13.ts
+++ day13.ts
@@ -1,17 +1,36 @@
 type ToTuple<
   N extends number,
   Res extends readonly any[] = []
-> = N extends Res["length"] ? Res : ToTuple<N, [...Res, any]>;
+> = N extends Res["length"] ? Res : ToTuple<N, [...Res, Res["length"]]>;
 
-type ToNumber<S> = S extends `${infer N extends number}` ? N : never;
-
-type DayCounter<Start extends number, End extends number> = ToNumber<
+type DayCounter<Start extends number, End extends number> =
   Exclude<
     ToTuple<End> extends infer R extends readonly any[]
-      ? keyof [any, ...R] & `${number}`
+      ? [...R, R["length"]][number]
       : never,
     ToTuple<Start> extends infer R extends readonly any[]
-      ? keyof R & `${number}`
+      ? R[number]
       : never
-  >
->;
+  >;
+
+namespace Alternative {
+  type ToTupleNever<
+    N extends number,
+    Res extends readonly never[] = []
+  > = N extends Res["length"] ? Res : ToTupleNever<N, [...Res, never]>;
+
+  type ToTuple<
+    Start extends number,
+    End extends number,
+    Res extends readonly any[]
+  > = End extends Res["length"]
+    ? [...Res, Res["length"]]
+    : ToTuple<Start, End, [...Res, Res["length"]]>;
+
+  type DayCounter<Start extends number, End extends number> =
+    ToTupleNever<Start> extends infer $N extends never[] // $N = ToTupleNever<Start>
+    ? ToTuple<Start, End, $N> extends infer Res extends readonly any[]
+      ? Res[number]
+      : never
+    : never;
+};
